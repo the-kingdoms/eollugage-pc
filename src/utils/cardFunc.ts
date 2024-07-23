f481d770ee91cf5cbc38a6c4fe89c09c9dbfe8f0
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-07-23 18:49:59 +0900

    fix: card func /utils로 분리

---
+++ src/utils/cardFunc.ts
@@ -0,0 +1,13 @@
+import { orderType, productType } from 'components/waitingOrderCard'
+
+export const returnTotalPrice = (orders: orderType[]) =>
+  orders.reduce((acc, order) => {
+    if (order.options) return acc + order.price + order.options?.reduce((acc2, option) => acc2 + option.price, 0)
+    return acc + order.price
+  }, 0)
+
+export const returnOptions = (options: productType[] | undefined) => {
+  if (options === undefined) return
+
+  return ' | '.concat(options?.map(option => `${option.name} (+${option.price}원)`).join(', '))
+}
