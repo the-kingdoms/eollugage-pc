e35c920be6d35f7faf1cf10c7e5c3f03efa4a3fd
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-07-23 14:43:06 +0900

    feat: 로그인 페이지 구현

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,56 @@
+import styled from 'styled-components'
+import { ReactComponent as Logo } from 'assets/image/eollugage-logo.svg'
+import { useNavigate } from 'react-router-dom'
+
 export default function Login() {
-  return <div>login page</div>
+  const navigate = useNavigate()
+
+  return (
+    <Container>
+      <img src={require('assets/image/login.png')} style={{ flex: 1 }} alt="login-image" />
+      <Right>
+        <Title>
+          <Logo />
+          간편하게 일하는 법
+        </Title>
+        <KakaoLoginButton onClick={() => navigate('/process')}>
+          <img src={require('assets/image/kakao.png')} width={24} height={24} />
+          카카오 로그인
+        </KakaoLoginButton>
+      </Right>
+    </Container>
+  )
 }
+
+const Container = styled.div`
+  display: flex;
+  overflow: hidden;
+`
+const Right = styled.div`
+  flex: 1;
+  background-color: black;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 88px;
+`
+const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #7c7c7c;
+  gap: 8px;
+`
+const KakaoLoginButton = styled.div`
+  background-color: #fee500;
+  width: 328px;
+  border-radius: 1000px;
+  padding: 20px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  cursor: pointer;
+`
