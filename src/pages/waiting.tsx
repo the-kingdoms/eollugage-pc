bbf710e5db14a1f2b0bc1b48c0482a3d1c52647a
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-09-01 22:55:04 +0900

    feat: 결제 완료 400 에러시 승인되지 않은 주문으로 scroll

--- src/pages/waiting.tsx
+++ src/pages/waiting.tsx
@@ -5,26 +5,37 @@ import { waitingCountAtom } from 'utils/atom'
 import { useGetPaymentHistory } from 'hooks/apis/paymentHistory'
 import { parseOrder } from 'utils/parseOrderDetail'
 import { sortOrder } from 'utils/order'
+import { useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 
 export default function WaitMain() {
+  const location = useLocation()
   const [waitingCount] = useAtom(waitingCountAtom)
 
   const { data: orderList } = useGetPaymentHistory()
+
+  useEffect(() => {
+    if (location.state !== null) {
+      document.getElementById(location.state)?.scrollIntoView({ behavior: 'smooth' })
+    }
+  }, [location.state])
 
   return (
     <Container>
       <TabTitle>승인 대기 {waitingCount}</TabTitle>
       <CardContainer>
         {sortOrder(orderList, 'PENDING').map((order, i) => (
-          <OrderCard
-            orderHistoryId={order.orderHistoryId}
-            paymentHistoryId={order.paymentHistoryId}
-            status={order.state}
-            time={order.createdAt}
-            tableNumber={order.tableNumber}
-            totalPrice={order.totalPrice}
-            orders={parseOrder(order.orderDetail)}
-          />
+          <div id={order.paymentHistoryId} key={order.orderHistoryId}>
+            <OrderCard
+              orderHistoryId={order.orderHistoryId}
+              paymentHistoryId={order.paymentHistoryId}
+              status={order.state}
+              time={order.createdAt}
+              tableNumber={order.tableNumber}
+              totalPrice={order.totalPrice}
+              orders={parseOrder(order.orderDetail)}
+            />
+          </div>
         ))}
       </CardContainer>
     </Container>
